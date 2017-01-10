c7d2d16a57eb9e3be51ecbc51b147f4e42465fbc
OpenShift System User <5847c0fd89f5cfe084000014@ex-std-node50.prod.rhcloud.com>
2017-01-10 03:32:37 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+PHP Wrapper to send [line notify](https://notify-bot.line.me) notification
+```
+$line = new \renlight10\LineNotify\Notification("token");
+$line->SETmessage("awesome!");
+echo $line->sendIt();
+```
+you can `SET` more property for example
+```
+$line->SETmessage("awesome!")->SETstickerPackageId(1)->SETstickerId(5);
+```
+see [documentation](https://notify-bot.line.me/doc/en/).
+
+ **NOTE :** currently direct image upload or ` imageFile ` not supported ~
+
+example response
+```
+{"header":{"Limit":1000,"ImageLimit":50,"Remaining":999,"ImageRemaining":50,"Reset":1483555725},"body":{"status":200,"message":"ok"}}
+```
---
+++ src/LineNotify.php
@@ -0,0 +1,62 @@
+<?php
+namespace renlight10\LineNotify;
+class Notification {
+	protected $token;
+	protected $message;
+	protected $imageThumbnail;
+	protected $imageFullsize;
+	protected $stickerPackageId;
+	protected $stickerId;
+	public function __construct($token) {
+		if(!isset($token)) {
+			throw new Exception("token couldn't be empty!");
+} else{
+$this->token=$token;
+}
+}
+public function __call($method, $arg){
+$allowmethod = substr($method,0,3);
+if($allowmethod==="SET"){
+$property = substr($method,3);
+if(property_exists($this, $property)){
+$this->$property = $arg[0];
+return $this;
+} else{
+throw new Exception("$property property not found!");
+return $this;
+}} else{
+throw new Exception("call to undefined method $method!");
+return $this;
+}
+}
+public function sendIt(){
+//memproses
+$url="https://notify-api.line.me/api/notify";
+			$ch = curl_init($url);
+			$header = array("Content-Type:application/x-www-form-urlencoded","Authorization:Bearer $this->token");
+			$body = http_build_query(get_object_vars($this));
+			$ch = curl_init($url);
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, $body);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+			curl_setopt($ch, CURLOPT_HEADER,1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			//mengolah
+			list($response_header, $response_body) = explode("\r\n\r\n",$result, 2);
+			unset($result);
+			$a_header=explode("\n",$response_header);
+			$_header=array();
+			foreach($a_header as $key=>$value) {
+				$data=explode(":",$value);
+				$_header[$data[0]]=$data[1];
+			}
+			unset($data);
+			unset($a_header);
+			$_body=json_decode($response_body,true);
+			//tampilkan hasil
+			$hasil=["header"=>["Limit"=>(int)$_header["X-RateLimit-Limit"],"ImageLimit"=>(int)$_header["X-RateLimit-ImageLimit"],"Remaining"=>(int)$_header["X-RateLimit-Remaining"],"ImageRemaining"=>(int)$_header["X-RateLimit-ImageRemaining"],"Reset"=>(int)$_header["X-RateLimit-Reset"]],"body"=>["status"=>(int)$_body["status"],"message"=>$_body["message"]]];
+			return json_encode($hasil);
+		}
+	}
+	?>
